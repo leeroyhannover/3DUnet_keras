7f47bd5c431c8b7c5a8fa4cde7670aaf77ecf0e0
leeroyhannover <leeroyhannover@126.com>
2022-09-07 13:12:48 +0200

    dataset august

--- dataGenerator.py
+++ dataGenerator.py
@@ -3,6 +3,7 @@
 
 import os
 import numpy as np
+from utils import *
 
 # load images
 def load_img(img_dir, img_list):
@@ -42,6 +43,100 @@ def imageLoader(img_dir, img_list, mask_dir, mask_list, batch_size):
             
             X = load_img(img_dir, img_list[batch_start:limit])
             Y = load_img(mask_dir, mask_list[batch_start:limit])
+            
+            yield(X,Y) # output the X and Y in batch size
+            
+            batch_start += batch_size # 都往后挪一个batch
+            batch_end += batch_size
+
+            
+# the deep 2D UNet requires different data size
+
+def imageLoaderDeep(img_dir, img_list, mask_dir, mask_list, batch_size):
+    L = len(img_list)
+    
+    # keras require generator to be infinite, so we use while true
+    while True:
+        
+        batch_start = 0
+        batch_end = batch_size
+        
+        while batch_start < L:
+            
+            limit = min(batch_end, L) # 考虑最后一个batch分割不完整的情况
+            
+            # X = load_img(img_list[batch_start:limit])
+            # Y = load_img(mask_list[batch_start:limit])
+            
+            X = load_img(img_dir, img_list[batch_start:limit])
+            Y = load_img(mask_dir, mask_list[batch_start:limit])
+            
+            # remove the last channel
+            X = np.squeeze(X)
+            Y = np.argmax(Y, axis=-1).astype('float64')
+            
+            yield(X,Y) # output the X and Y in batch size
+            
+            batch_start += batch_size # 都往后挪一个batch
+            batch_end += batch_size
+            
+
+# generator for 3D U-Net
+def imageLoader3D(img_dir, img_list, mask_dir, mask_list, batch_size):
+    L = len(img_list)
+    
+    # keras require generator to be infinite, so we use while true
+    while True:
+        
+        batch_start = 0
+        batch_end = batch_size
+        
+        while batch_start < L:
+            
+            limit = min(batch_end, L) # 考虑最后一个batch分割不完整的情况
+            
+            # X = load_img(img_list[batch_start:limit])
+            # Y = load_img(mask_list[batch_start:limit])
+            
+            X = load_img(img_dir, img_list[batch_start:limit])
+            Y = load_img(mask_dir, mask_list[batch_start:limit])
+            
+            # remove the last channel
+            Y = np.argmax(Y, axis=-1).astype('float64') 
+            Y = np.expand_dims(Y, axis=4) # keep the shape [None, W, H, D, CH=1]
+            
+            yield(X,Y) # output the X and Y in batch size
+            
+            batch_start += batch_size # 都往后挪一个batch
+            batch_end += batch_size
+            
+
+# generator for 3D U-Net. pad slices to stack
+def imageLoader3DSlice(img_dir, img_list, mask_dir, mask_list, batch_size):
+    L = len(img_list)
+    
+    # keras require generator to be infinite, so we use while true
+    while True:
+        
+        batch_start = 0
+        batch_end = batch_size
+        
+        while batch_start < L:
+            
+            limit = min(batch_end, L) # 考虑最后一个batch分割不完整的情况
+            
+            # X = load_img(img_list[batch_start:limit])
+            # Y = load_img(mask_list[batch_start:limit])
+            
+            X = load_img(img_dir, img_list[batch_start:limit])
+            Y = load_img(mask_dir, mask_list[batch_start:limit])
+            
+            # remove the last channel
+            Y = np.argmax(Y, axis=-1).astype('float64') 
+            Y = np.expand_dims(Y, axis=4) # keep the shape [None, W, H, D, CH=1]
+            
+            # image to slice to stack
+            X = slice2stack(X[:,0:63:4,...], [1,1,0.25]) 
             
             yield(X,Y) # output the X and Y in batch size
             
--- preData.py
+++ preData.py
@@ -16,6 +16,8 @@ import glob
 
 import splitfolders  # use to segment the data
 
+from natsort import natsorted
+
 
 # read into the data
 def readMRC(path):
@@ -37,16 +39,22 @@ def visusalizeIMG(n_slice, test_img, test_msk):
     plt.title('GT slice')
     plt.show()
 
+# normalize into [0, 1]
+def normStack(imageStack):
+    Range = np.max(imageStack) - np.min(imageStack)
+    normTemp = ((imageStack - np.min(imageStack))/Range - 0.5) * 2 
+    
+    return (normTemp +1)/2 
+    
 
 if __name__ == "__main__":
     
-    PATH = 'H:/My Drive/rawData/MDC_HZDR/simulation_20220715/'
+    PATH = 'H:/My Drive/rawData/MDC_HZDR/simulation_20220818/'
     parser = argparse.ArgumentParser()
     parser.add_argument('--img_dir', default= PATH + 'focal/', help='path to the images')
     parser.add_argument('--mask_dir', default= PATH + 'GT/', help='path to the labels')
     parser.add_argument('--crop_size', default=(18, 82), help='crop the image to enhance SNR')
-    parser.add_argument('--threshold', default=0.825, help='threshold the normalized masks')
-
+    parser.add_argument('--threshold', type=float, default=0.825, help='threshold the normalized masks')
     
     # parser.add_argument('--split_val', default=20, help='number of images for validation')
     # parser.add_argument('--split_test', default=19, help='number of images for testing')
@@ -55,13 +63,13 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     
-    if not os.path.isdir('./preData_3channels'):
-        os.mkdir('./preData_3channels')
+    if not os.path.isdir('./preData_08_2022'):
+        os.mkdir('./preData_08_2022')
         
-    SAVED_PATH = 'E:/EM/demo/3D_Unet_keras/preData_3channels/'
+    SAVED_PATH = 'E:/EM/demo/3D_Unet_keras/preData_08_2022/'
         
-    img_list = sorted(glob.glob(args.img_dir + '/*focal.mrc'))
-    msk_list = sorted(glob.glob(args.mask_dir + '/*gt.mrc'))
+    img_list = natsorted(glob.glob(args.img_dir + '/*focal.mrc'))
+    msk_list = natsorted(glob.glob(args.mask_dir + '/*gt.mrc'))
     
     
     num_images = len(img_list)
@@ -72,19 +80,25 @@ if __name__ == "__main__":
         temp_img = readMRC(img_list[num])
         temp_msk = readMRC(msk_list[num])
         
-        # rescale into [0,1]
-        temp_img = np.interp(temp_img, (temp_img.min(), temp_img.max()), (0, 1))
-        temp_msk = np.interp(temp_msk, (temp_msk.min(), temp_msk.max()), (0, 1))
+        # # rescale into [0,1]
+        # temp_img = np.interp(temp_img, (temp_img.min(), temp_img.max()), (0, 1))
+        # temp_msk = np.interp(temp_msk, (temp_msk.min(), temp_msk.max()), (0, 1))
+        
+        # normalize the data [0, 1]
+        temp_img = normStack(temp_img)
+        temp_msk = normStack(temp_msk)
         
         # binary the mask
-        temp_msk = (temp_msk < args.threshold).astype(int)
+        # temp_msk = (temp_msk < args.threshold).astype(int)  # for the 07.2022 data < 0.825
+        temp_msk = (temp_msk > args.threshold).astype(int)  # for the 08.2022 data > 0.45
         
         # crop the stack 
         temp_msk = temp_msk[args.crop_size[0]:args.crop_size[1], args.crop_size[0]:args.crop_size[1], args.crop_size[0]:args.crop_size[1]]
         temp_img = temp_img[args.crop_size[0]:args.crop_size[1], args.crop_size[0]:args.crop_size[1], args.crop_size[0]:args.crop_size[1]]
         
         # adjust the data format
-        temp_img = np.stack([temp_img, temp_img, temp_img], axis = 3)
+        # temp_img = np.stack([temp_img, temp_img, temp_img], axis = 3) # use for transfer learning in 3 channels
+        temp_img = np.stack([temp_img], axis = 3)
         temp_msk = to_categorical(temp_msk, num_classes=2) 
         
         # save file
@@ -99,7 +113,8 @@ if __name__ == "__main__":
             print("too low SNR:", num)
     
     # split data into training, validation and testing
-    OUTPUT_PATH = 'E:/EM/demo/3D_Unet_keras/inputData_3channels/'
-    splitfolders.ratio(SAVED_PATH, output=OUTPUT_PATH, seed=42, ratio=(.8, .1, .1), group_prefix=None) # default values
+    OUTPUT_PATH = 'E:/EM/demo/3D_Unet_keras/data/2022_08/inputData/'
+    # splitfolders.ratio(SAVED_PATH, output=OUTPUT_PATH, seed=42, ratio=(.8, .1, .1), group_prefix=None) # default values
+    splitfolders.ratio(SAVED_PATH, output=OUTPUT_PATH, seed=42, ratio=(.8, .2), group_prefix=None)  # test will be manually assigned
 
         
